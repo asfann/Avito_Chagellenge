861ed625357fafc30304d263d6ba92b9131bd393
asfann <64355174+asfann@users.noreply.github.com>
2022-10-27 00:14:42 +0600

    cleaning

--- Avito_Chagellenge/Managers/ApiCaller.swift
+++ Avito_Chagellenge/Managers/ApiCaller.swift
@@ -12,7 +12,6 @@ struct Constants {
     static let base_URL = "https://run.mocky.io/v3/1d1cb4ec-73db-4762-8c4b-0b8aa3cecd4c"
 }
 
-let cachy = CachyLoader()
 
 enum APIError: Error {
     case failedToGetData
